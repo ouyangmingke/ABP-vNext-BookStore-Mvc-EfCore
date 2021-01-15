83ddd091f80b1499c089b788f9e3313e0295f7e9
1120108 <yangmingke@cxzdh.cn>
2021-01-15 17:18:49 +0800

    更新EFC理解

---
+++ src/Acme.BookStore.Domain/Books/IBookRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Volo.Abp.Domain.Repositories;
+
+namespace Acme.BookStore.Books
+{
+    /// <summary>
+    /// 扩展标准IRepository<Book, Guid>接口，
+    /// </summary>
+    public interface IBookRepository : IRepository<Book, Guid>
+    {
+        Task<List<Book>> GetAllListAsync();
+    }
+}
